0ca499f4296e097c74cfb57ee73d41f575a2efdc
osneida <osneidabordones@gmial.com>
2023-10-16 14:55:55 -0400

    certificates

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CertificateController;
 use App\Http\Controllers\Api\PayrollController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
